f7e83c4b25c497bc275eedfc0acc3f255ddd59a5
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-07-18 11:38:11 -0500

    Shawn's solution - database migrated to sqlite

--- craigslist_jr_proj/settings.py
+++ craigslist_jr_proj/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'craigslist_jr_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_cl',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
